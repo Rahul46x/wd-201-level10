7310f72536731d5dedea024062ea028e9fefa51e
gupta56x <113337289+gupta56x@users.noreply.github.com>
2022-12-24 15:11:08 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate() {
+      // define association here
     }
 
     static addTodo({title, dueDate}) {
@@ -67,10 +68,6 @@ module.exports = (sequelize, DataTypes) => {
     }
     setCompletionStatus(receiver) {
       return this.update({ completed: receiver });
-    }
-
-    markAsCompleted() {
-      return this.update({completed: true});
     }
     
   }
